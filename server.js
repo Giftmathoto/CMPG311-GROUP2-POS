ab032ce7e5467db6474927bba7ee5e1178e5bf60
GEE MATHOTO <45283486@adt.nwu.ac.za>
2026-05-17 21:25:17 +0200

    server and login

--- server.js
+++ server.js
@@ -7,26 +7,28 @@ const productsRoutes = require('./routes/products');
 const salesRoutes = require('./routes/sales');
 const employeesRoutes = require('./routes/employees');
 const loyaltyRoutes = require('./routes/loyalty');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const authRoutes = require('./routes/auth');
 
-app.use('/api/auth', authRoutes);
+// middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// serve frontend
+// frontend
 app.use(express.static(path.join(__dirname, 'public')));
 
-// API routes
+// routes
+app.use('/api/auth', authRoutes);
 app.use('/api/customers', customersRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/sales', salesRoutes);
 app.use('/api/employees', employeesRoutes);
 app.use('/api/loyalty', loyaltyRoutes);
 
-// dashboard default
+// default page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
